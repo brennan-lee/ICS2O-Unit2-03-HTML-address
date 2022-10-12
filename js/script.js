92343cd019dad1638157537746bb496609551143
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-10-12 14:00:15 -0400

    start

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2020 Brennan Lee All rights reserved
+//
+// Created by: Brennan Lee
+// Created on: oct 2022
+// This file contains the JS functions for index.html
+
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
+ function enterClicked() {
+    // input
+    const street Number = document.getElementById("first-name").value
+    const street = parseInt(document.getElementById("age-entered").value)
+  
+    // output
+    document.getElementById("address").innerHTML =
+      "Your info is: " + street Number + ", street " + userAge + "."
+  }
+  
