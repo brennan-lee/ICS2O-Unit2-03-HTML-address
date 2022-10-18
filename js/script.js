cc3a003e14080a63ff568b2586ef3862dd3c8a2e
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-10-18 14:50:35 -0400

    done

--- js/script.js
+++ js/script.js
@@ -1,18 +1,8 @@
-// Copyright (c) 2020 Brennan Lee All rights reserved
-//
-// Created by: Brennan Lee
-// Created on: oct 2022
-// This file contains the JS functions for index.html
-
-/**
- * This function gets users sreet and sreet number and shows it back to user.
- */
 function enterClicked() {
-  // input
-  const streetNumber = parseInt(document.getElementById("street-number").value)
-  const streetName = document.getElementById("street-name").value
 
-  // output
+  const street = document.getElementById("street").value
+  const streetNumber = parseInt(document.getElementById("street number-entered").value)
+
   document.getElementById("address").innerHTML =
-    "Your info is: " + streetName + "," + streetNumber + "."
+    "Your info is: " + street + " " + streetNumber + "."
 }
