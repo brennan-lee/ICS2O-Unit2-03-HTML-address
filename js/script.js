bbad21e47427a82dd440b70ebe467ad24c42ebfd
brennan-lee <brennan-lee@users.noreply.github.com>
2022-10-18 18:52:33 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 function enterClicked() {
-
   const street = document.getElementById("street").value
-  const streetNumber = parseInt(document.getElementById("street number-entered").value)
+  const streetNumber = parseInt(
+    document.getElementById("street number-entered").value
+  )
 
   document.getElementById("address").innerHTML =
     "Your info is: " + street + " " + streetNumber + "."
